4af6ce2446fbbe0c2062e3fabbdeeed2f9aead4e
Aditya Oberai <424@doonschool.com>
2020-08-15 19:49:37 +0530

    Added Xamarin.Forms App

---
+++ GarudaHacks - Xamarin/GarudaHacks/GarudaHacks/ImageCapture.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using Plugin.Media;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using System.Xml.XPath;
+
+namespace GarudaHacks
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImageCapture : ContentPage
+    {
+        public ImageCapture()
+        {
+            InitializeComponent();
+        }
+        private async void clickImage_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera Available", "Function To Be Built Yet", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                SaveToAlbum = true,
+                //Directory = "Sample",
+                //Name = "test.jpg",
+                AllowCropping = false
+            });
+
+            if (file == null)
+                return;
+
+            //await DisplayAlert("File Location", file.Path, "OK");
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        private async void sendToCodeSpace_Clicked(object sender, EventArgs e)
+        {
+            if(langPicker.SelectedItem==null)
+            {
+                await DisplayAlert("Language Not Selected!!!","Go Pick A Language!!!", "OK");
+                return;
+            }
+
+            if (Connectivity.NetworkAccess == NetworkAccess.None)
+            {
+                await DisplayAlert("No Network Available","Please Connect To Your Wifi Or Turn on Mobile Data","OK");
+            }
+
+            //await DisplayAlert("Send To CodeSpace Button Works!!!","Function To Be Built Yet\n\nLanguage Selected => "+langPicker.SelectedItem,"OK");
+
+            MakeRequest();
+        }
+
+        private async void selectImage_Clicked(object sender, EventArgs e)
+        {
+            //Debug.WriteLine("Select The Image Button Works!!!");
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Photos Not Supported",
+                           "Sorry! Permission not granted to photos.", "OK");
+                //return null;
+            }
+
+            var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new
+                Plugin.Media.Abstractions.PickMediaOptions{ });
+
+            //await DisplayAlert("File Location", file.Path, "OK");
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        private async void MakeRequest()
+        {
+            var person = new Models.Test();
+            person.Name = "John Doe";
+            person.Occupation = "gardener";
+
+            var json = JsonConvert.SerializeObject(person);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var url = "https://httpbin.org/post";
+            var client = new HttpClient();
+
+            var response = await client.PostAsync(url, data);
+
+            string result = response.Content.ReadAsStringAsync().Result;
+            if(result!=null)
+                await DisplayAlert("Success","API Call Successfully Made","OK");
+        }
+    }
+}
---
+++ GarudaHacks - Xamarin/GarudaHacks/GarudaHacks/Models/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarudaHacks.Models
+{
+    class Test
+    {
+        public string Name { get; set; }
+        public string Occupation { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Occupation}";
+        }
+    }
+}
